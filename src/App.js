8210b3e6e819dc47ad36d18b402138d5386dab7b
JamieB92 <j.bonner6992@gmail.com>
2023-10-31 21:49:02 +0000

    create Navbar, Route, Style with bootstrap and custom css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import Button from 'react-bootstrap/Button';
-import './App.css';
+import { Container } from 'react-bootstrap';
+import styles from './App.module.css';
+import NavBar from './components/NavBar';
+import { Route, Switch } from 'react-router-dom'
+
+
 
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="primary">Primary</Button>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/signin" render={() => <h1>Sign In</h1> } />
+        </Switch>
+        <Switch>
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1> } />
+        </Switch>
+        <Switch>
+          <Route render={() => <p>Page Not Found</p> } />
+        </Switch>
+      </Container>
     </div>
   );
 }
