b59c51cf7f5996a376bf3af60347e07a88cffd61
JamieB92 <j.bonner6992@gmail.com>
2023-11-01 21:25:51 +0000

    create and style a functioning signup form

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Container } from 'react-bootstrap';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Route, Switch } from 'react-router-dom'
-
-
+import './api/axiosDefaults'
+import SignUpForm from './pages/auth/SignUpForm';
 
 
 function App() {
@@ -13,14 +13,8 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
-        </Switch>
-        <Switch>
           <Route exact path="/signin" render={() => <h1>Sign In</h1> } />
-        </Switch>
-        <Switch>
-          <Route exact path="/signup" render={() => <h1>Sign Up</h1> } />
-        </Switch>
-        <Switch>
+          <Route exact path="/signup" render={() => < SignUpForm/>} />
           <Route render={() => <p>Page Not Found</p> } />
         </Switch>
       </Container>
---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+// Deployed api url 
+axios.defaults.baseURL = 'https://drf-moments-3bf5a8310302.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
