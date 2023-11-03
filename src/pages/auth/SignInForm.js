3c5d3f6def2cb39447adfe1d908f8aa666623e4b
JamieB92 <j.bonner6992@gmail.com>
2023-11-03 19:39:01 +0000

    fix alert display error

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -21,13 +21,11 @@ function SignInForm() {
     username: "",
     password: "",
   });
-
   const { username, password } = signInData;
 
   const [errors, setErrors] = useState({});
 
   const history = useHistory();
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -37,12 +35,14 @@ function SignInForm() {
       setErrors(err.response?.data);
     }
   };
+
   const handleChange = (event) => {
     setSignInData({
       ...signInData,
       [event.target.name]: event.target.value,
     });
   };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
@@ -61,10 +61,11 @@ function SignInForm() {
               />
             </Form.Group>
             {errors.username?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
+              <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
+
             <Form.Group controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
@@ -76,17 +77,16 @@ function SignInForm() {
                 onChange={handleChange}
               />
             </Form.Group>
-            {errors.username?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
+            {errors.password?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
-
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
               type="submit"
             >
-              Sign In
+              Sign in
             </Button>
             {errors.non_field_errors?.map((message, idx) => (
               <Alert key={idx} variant="warning" className="mt-3">
